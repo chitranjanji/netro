ec08ff4e54706b1e048242593a34a5b87b4188e0
chitranjanji <chitranjanharode@gmail.com>
2023-10-17 11:45:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+ function hii(){
+   gsap.to(".over",{
+      // height:0,
+      y:-40,
+      opacity:0,
+      delay:1,
+      duration:1
+   })
+   gsap.to(".over1",{
+      // height:0,
+      y:-40,
+      opacity:0,
+      delay:1,
+      duration:1
+   })
+   gsap.to(".over2",{
+      // height:0,
+      y:-40,
+      opacity:0,
+      delay:1,
+      duration:1
+   })
+   gsap.to(".over3",{
+      // height:0,
+      y:-40,
+      opacity:0,
+      delay:1,
+      duration:1
+   })
+   // gsap.to("#overlay",{
+   //    y:"-100%",
+   //    opacity:0,
+   //    delay:1,
+   //    duration:1.5
+   // })
+
+ }
+ hii();
+ 
